a4e8c10466012e23f025e3889942ae3c79ec8ee2
Explorer <propokerstar90@gmail.com>
2018-11-09 22:41:09 +0200

    Binary search

--- main.js
+++ main.js
@@ -4,18 +4,24 @@ function findNum(arr, elem){
     let start = 0;
     let end = arr.length -1;
     let middle = Math.floor((start - end) / 2);
-    while(arr[middle] !== elem){
+    while(arr[middle] !== elem && start <= end){
         if(arr[middle] > elem){
-            end = middle - 1
+            end = middle - 1;
         }
         else{
-            start = middle + 1
+            start = middle + 1;
         }
-        middle = Math.floor(start + end) / 2
+        middle = Math.floor((start + end) / 2);
     }
-    return middle
+
+    return arr[middle] === elem ? middle : -1;
+
+    /*if(arr[middle] === elem){
+        return middle
+    }
+    return -1;*/
 }
 
 let testArr = [1,3,4,5,9,12,13,14,17,20,33];
 
-console.log(findNum(testArr, 17));
+console.log(findNum(testArr, 9));
