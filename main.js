00d28961801b72220776504ab959d86d7f485c58
Explorer <propokerstar90@gmail.com>
2018-11-14 23:10:24 +0200

    Sorting

--- main.js
+++ main.js
@@ -34,4 +34,44 @@ function substringSearch(long, short) {
     return count;
 }
 
-console.log(substringSearch('some test string', 'te'));
+// Bubble sort
+
+function bubbleSort(arr) {
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--){
+        noSwaps = true;
+        for(let j = 0; j < i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+                noSwaps = false
+            }
+        }
+        if(noSwaps) break;
+    }
+    return arr
+}
+
+// Another sort
+
+function sortArr(arr) {
+    for(let i = 0; i<arr.length; i++){
+        let lowest = i;
+        for(let j = i+1; j<arr.length; j++){
+            if(arr[i] < arr[lowest]){
+                lowest = j;
+            }
+        }
+        if(i !== lowest){
+            let temp = arr[i];
+            arr[i] = arr[lowest];
+            arr[lowest] = temp;
+        }
+    }
+    return arr
+}
+
+let testArray = [12, 1, 10, 4, 5, 3, 44];
+
+console.log(sortArr(testArray));
