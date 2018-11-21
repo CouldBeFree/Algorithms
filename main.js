3fbdfdf27eabd76f67b5177358e7409ec5101674
Sasha Zoria <alex.zoria@n3wnormal.com>
2018-11-21 17:52:03 +0200

    capitalize

--- main.js
+++ main.js
@@ -250,4 +250,17 @@ function simpleAdding(num) {
     return result
 }
 
-console.log(simpleAdding(12));
+// console.log(simpleAdding(12));
+
+// Capitalize first letter
+
+function letterCapitalize(str) {
+    let pieces = str.split(" ");
+    for ( let i = 0; i < pieces.length; i++ ) {
+        let j = pieces[i].charAt(0).toUpperCase();
+        pieces[i] = j + pieces[i].substr(1).toLowerCase();
+    }
+    return pieces.join(" ");
+}
+
+console.log(letterCapitalize('hello world'));
